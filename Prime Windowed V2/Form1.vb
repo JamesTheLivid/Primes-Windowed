06d8a139703392a890bd95f0c68e50fca0acbc4d
James Robinson <robinsonj@burgate.hants.sch.uk>
2017-12-06 17:10:50 +0000

    Added all files so far from Visual Studio

---
+++ Prime Windowed V2/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class formMainWindow
+    Private Sub tabAbout_Click(sender As Object, e As EventArgs) Handles tabAbout.Click
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+End Class
